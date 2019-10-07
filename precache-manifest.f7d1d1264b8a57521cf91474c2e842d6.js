c16de008c27ee03dfb2937a3ed0d91cd472bdb97
Meetanshi Mittal <meetanshi.18@gmail.com>
2019-10-08 00:11:41 +0530

    Updates

---
+++ precache-manifest.f7d1d1264b8a57521cf91474c2e842d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4066504232bb435c794dfb8ba646b3",
+    "url": "/meetanshi_mittal/index.html"
+  },
+  {
+    "revision": "86fcc65a906e8719b720",
+    "url": "/meetanshi_mittal/static/css/main.4a480d38.chunk.css"
+  },
+  {
+    "revision": "8e1d7594879236e58f20",
+    "url": "/meetanshi_mittal/static/js/2.7b426b8b.chunk.js"
+  },
+  {
+    "revision": "86fcc65a906e8719b720",
+    "url": "/meetanshi_mittal/static/js/main.e9110757.chunk.js"
+  },
+  {
+    "revision": "bba6b33d6f6f550e5d9c",
+    "url": "/meetanshi_mittal/static/js/runtime~main.ce1e63be.js"
+  },
+  {
+    "revision": "508d01b9a243b2888dca28e7dff596a8",
+    "url": "/meetanshi_mittal/static/media/jeans.508d01b9.jpg"
+  },
+  {
+    "revision": "da4c83b5f2584c28a9d21ae487b4eb45",
+    "url": "/meetanshi_mittal/static/media/professionalPhotoSquare.da4c83b5.jpg"
+  }
+]);
